e3574c71830af02281df7f7d9d5852d85d3469f0
vinu <mathewzchrh603@gmail.com>
2024-08-24 21:06:10 +0530

    changes

--- Asturah_app/urls.py
+++ Asturah_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', home_page),
+    path('', home_page,name="home"),
     path('products', products_page,name="products"),
     path('about_us', about_us,name="about_us"),
 ]
